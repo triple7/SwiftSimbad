2cc7254af2053243d2c5ac2b0c482644d22787f6
Yuma Antoine Decaux <yuma@astreos.space>
2025-01-11 19:02:25 +1000

    changed json response model to metadta dict and data array of strings only

--- Sources/SwiftSimbad/SwiftSimbad+requests.swift
+++ Sources/SwiftSimbad/SwiftSimbad+requests.swift
@@ -56,78 +56,15 @@ extension SwiftSimbad {
 
         let task = session.dataTask(with: request) { [weak self] data, response, error in
             
-            var result = SimbadResponse()
             if self!.requestIsValid(error: error, response: response) {
                 print(String(data: data!, encoding: .utf8))
-                switch table {
-                case .allfluxes:
-                result.setAllfluxes(try! JSONDecoder().decode(AllfluxesResponse.self, from: data!))
-                case .alltypes:
-                result.setAlltypes(try! JSONDecoder().decode(AlltypesResponse.self, from: data!))
-                case .author:
-                result.setAuthor(try! JSONDecoder().decode(AuthorResponse.self, from: data!))
-                case .basic:
-                result.setBasic(try! JSONDecoder().decode(BasicResponse.self, from: data!))
-                case .biblio:
-                result.setBiblio(try! JSONDecoder().decode(BiblioResponse.self, from: data!))
-                case .cat:
-                result.setCat(try! JSONDecoder().decode(CatResponse.self, from: data!))
-                case .filter:
-                result.setFilter(try! JSONDecoder().decode(FilterResponse.self, from: data!))
-                case .flux:
-                result.setFlux(try! JSONDecoder().decode(FluxResponse.self, from: data!))
-                case .h_link:
-                result.setH_link(try! JSONDecoder().decode(H_linkResponse.self, from: data!))
-                case .has_ref:
-                result.setHas_ref(try! JSONDecoder().decode(Has_refResponse.self, from: data!))
-                case .ident:
-                result.setIdent(try! JSONDecoder().decode(IdentResponse.self, from: data!))
-                case .ids:
-                result.setIds(try! JSONDecoder().decode(IdsResponse.self, from: data!))
-                case .journals:
-                result.setJournals(try! JSONDecoder().decode(JournalsResponse.self, from: data!))
-                case .keywords:
-                result.setKeywords(try! JSONDecoder().decode(KeywordsResponse.self, from: data!))
-                case .mesDiameter:
-                result.setMesDiameter(try! JSONDecoder().decode(MesDiameterResponse.self, from: data!))
-                case .mesDistance:
-                result.setMesDistance(try! JSONDecoder().decode(MesDistanceResponse.self, from: data!))
-                case .mesFe_h:
-                result.setMesFe_h(try! JSONDecoder().decode(MesFe_hResponse.self, from: data!))
-                case .mesHerschel:
-                result.setMesHerschel(try! JSONDecoder().decode(MesHerschelResponse.self, from: data!))
-                case .mesISO:
-                result.setMesISO(try! JSONDecoder().decode(MesISOResponse.self, from: data!))
-                case .mesIUE:
-                result.setMesIUE(try! JSONDecoder().decode(MesIUEResponse.self, from: data!))
-                case .mesPLX:
-                result.setMesPLX(try! JSONDecoder().decode(MesPLXResponse.self, from: data!))
-                case .mesPM:
-                result.setMesPM(try! JSONDecoder().decode(MesPMResponse.self, from: data!))
-                case .mesRot:
-                result.setMesRot(try! JSONDecoder().decode(MesRotResponse.self, from: data!))
-                case .mesSpT:
-                result.setMesSpT(try! JSONDecoder().decode(MesSpTResponse.self, from: data!))
-                case .mesVar:
-                result.setMesVar(try! JSONDecoder().decode(MesVarResponse.self, from: data!))
-                case .mesVelocities:
-                result.setMesVelocities(try! JSONDecoder().decode(MesVelocitiesResponse.self, from: data!))
-                case .mesXmm:
-                result.setMesXmm(try! JSONDecoder().decode(MesXmmResponse.self, from: data!))
-                case .mes_otype:
-                result.setMes_otype(try! JSONDecoder().decode(Mes_otypeResponse.self, from: data!))
-                case .otypedef:
-                result.setOtypedef(try! JSONDecoder().decode(OtypedefResponse.self, from: data!))
-                case .otypes:
-                result.setOtypes(try! JSONDecoder().decode(OtypesResponse.self, from: data!))
-                case .ref:
-                result.setRef(try! JSONDecoder().decode(RefResponse.self, from: data!))
-                }
-                
+                let result = try! JSONDecoder().decode(SimbadResponse.self, from: data!)
+
                 self?.sysLog.append(SimbadSyslog(log: .OK, message: "query \(simbadRequest.getSelectQuery()) result downloaded"))
+                closure(result)
+                return
+
             }
-            closure(result)
-            return
         }
         task.resume()
     }
